f6b50d0c8d6712bef8ab693eccdffbb320de923c
Rikard Teodorsson <rikardt@student.chalmers.se>
2017-07-08 13:40:19 +0200

    Better

--- app/src/main/java/se/swecookie/passwordstrength/MainActivity.java
+++ app/src/main/java/se/swecookie/passwordstrength/MainActivity.java
@@ -1,19 +1,26 @@
 package se.swecookie.passwordstrength;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        WebView view = new WebView(this);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl("file:///android_asset/index.html");
-        setContentView(view);
+        loadWebView();
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void loadWebView() {
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+        //setContentView(view);
     }
 }
