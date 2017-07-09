c7417c5fcd3b9ab36895f411d9c28ed4f8e7f53d
Rikard Teodorsson <rikardt@student.chalmers.se>
2017-07-09 13:17:40 +0200

    Added about dialog

--- app/src/main/java/se/swecookie/passwordstrength/MainActivity.java
+++ app/src/main/java/se/swecookie/passwordstrength/MainActivity.java
@@ -1,12 +1,14 @@
 package se.swecookie.passwordstrength;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +20,51 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("SetJavaScriptEnabled")
     private void loadWebView() {
-        webView = (WebView) findViewById(R.id.webview);
+        WebView webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/index.html");
-        //setContentView(view);
+    }
+
+    public void onButtonClicked(View view) {
+        switch (view.getId()) {
+            case R.id.txtAbout:
+                showAbout();
+                break;
+        }
+    }
+
+    private void showAbout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("About");
+        builder.setMessage(getString(R.string.about_message));
+        builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setNeutralButton("License", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                showLicenseDialog();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void showLicenseDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Script License");
+        builder.setMessage(getString(R.string.license));
+        builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
